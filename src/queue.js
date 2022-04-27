a83ddfc9e97a573cfec974575958421058ddf392
Julia Filippova <julia.94@bk.ru>
2022-04-27 22:28:08 +0300

    feat: task Queue

--- src/queue.js
+++ src/queue.js
@@ -13,22 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+ class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;//ссылка на начало очереди
+    this.tail = null;//ссылка на конец очереди
+    this.length = 0;//количество элементов
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {// возвращает **связный список**, лежащий в основе данной **очереди**
+    return this.head;
   }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {//— помещает `value` в конец **очереди**
+    const node = new ListNode(value)//создаем очередной элеменнт
+    if(this.head){// если какойто элемент есть в очереди, то 
+      this.tail.next = node;//следующий за последним будет наш новосозданный узел
+      this.tail = node;//tail указывает на новосозданный элемент
+    }else {// если элементов не было
+      this.head = node;//ты будешь первым
+      this.tail = node;// и последним
+    }
+    this.length++;//увеличение длины очереди
   }
+
+  dequeue() {//извлекает значение с начала **очереди** и удаляет его
+    const current = this.head//ссылка на текущий элемент
+    this.head = this.head.next//теперь начало это следующий элемент
+    this.length--;//уменьшаем длину очереди
+    return current.value;//возвращаем его значение
+}
 }
 
 module.exports = {
